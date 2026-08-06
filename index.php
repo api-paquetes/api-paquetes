04b6e66712893eba2a0bd8fda89f190909950fd2
api-paquetes <sdinanlleli@gmail.com>
2026-08-06 16:23:58 -0400

    Fix header content type in index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-header('Content-Type: text/html; charset=UTF-8');
+header('Content-Type: texto/html; charset=UTF-8');
 
 echo "<h1>API de Viajes funcionando</h1>";
 echo "<a href='api.php'>Ver Paquetes en JSON</a>";
 
-// Esto le dice a Render que sí estamos vivos
+// Esto le dice a Render que si estamos vivos
 $port = $_ENV['PORT'] ?? 8000;
 error_log("API corriendo en puerto: " . $port);
 ?>
